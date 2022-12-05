ba136138931ee1ebc6ef61f240e7a349bae446e0
joanarijo <joanarijodesign@gmail.com>
2022-12-05 04:47:45 +0000

    day 2 - part 1

--- 2022/1/result.js
+++ 2022/1/result.js
@@ -3,21 +3,21 @@ let calories = fs.readFileSync('input.txt', 'utf8').split('\n');
 calories = calories.map((element) => element.trim());
 
 
-let result_part1 = [];
+let resultPart1 = [];
 let temp = 0;
 
 calories.forEach(el => {
     if (!el) {
-        result_part1.push(temp);
+        resultPart1.push(temp);
         temp = 0;
     } else {
         temp += parseInt(el);
     }
 });
 
-result_part1.push(temp);       
-console.log("Max calories: ", Math.max(...result_part1));
+resultPart1.push(temp);       
+console.log("Max calories: ", Math.max(...resultPart1));
         
 
-result_part2 = result_part1.sort((a,b)=>b-a);
-console.log("Total Calories: ", result_part2[0] + result_part2[1] + result_part2[2]);
+resultPart2 = resultPart1.sort((a,b)=>b-a);
+console.log("Total Calories: ", resultPart2[0] + resultPart2[1] + resultPart2[2]);
